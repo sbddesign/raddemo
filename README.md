6731d3ae6ac6cbd589475b3850a1da4e53ef87fb
sbddesign <stephen@d.elor.me>
2019-01-14 18:05:56 -0500

    Built basic functionality of RadDemo

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# raddemo
+# RadDemo
+
+RadDemo is a simple tool for creating demonstrations orr presentations out of video files.
+
+## Basic Steps
+
+1. Make a cool looking animated video for your presentation.
+2. Define timecodes where you want the video to automatically pause in your presentation.
+3. Load your demo video with the `<video>`, plug your pause point timecodes into RadDemo, and present.
+
+## Credit
+
+This project utilizes KeyboardJS for key binding. https://github.com/RobertWHurst/KeyboardJS
---
+++ assets/js/raddemo.js
@@ -0,0 +1,95 @@
+function radDemo(pausePoints){
+    //Variables
+    var video = document.getElementById("demo-video");
+    var interval = 200;
+    if(typeof pausePoints !== 'object') {
+        var pausePoints = [
+            2,
+            4,
+            6,
+            8,
+            10
+        ];
+    }
+    var reportCurrentTime,
+        prevPausePoint,
+        currentPausePoint,
+        nextPausePoint,
+        currentTime;
+
+    //Function that starts the RadDemo
+    function startRadDemo(video){
+        video.play();
+        if(!video.paused) {
+            reportCurrentTime = setInterval(function(){
+                if(video.readyState > 0) {
+                    currentTime = video.currentTime;
+
+                    pausePoints.forEach(function(value, index){
+
+                        // console.log(index);
+                        //If current time is within .1s of this pause point
+                        if( currentTime > value - .1 && currentTime < value + .1) {
+                            stopRadDemo(video);
+                            redefinePausePoints(index)
+                        }
+                    });
+                }
+            }, interval);
+        }
+    }
+
+    //Function that stops the RadDemo
+    function stopRadDemo(video){
+        video.pause();
+        clearInterval(reportCurrentTime);
+    }
+
+    function restartRadDemo(video){
+        stopRadDemo(video);
+        startRadDemo(video);
+    }
+
+    //Function that toggles the RadDemo
+    function toggleRadDemo(video) {
+        if(video.paused) {
+            startRadDemo(video);
+        } else {
+            stopRadDemo(video);
+        }
+    }
+
+    function navigateRadDemo(video, destination) {
+
+        if(pausePoints[destination] === undefined) {
+            video.currentTime = 0;
+            redefinePausePoints(0);
+        } else {
+            video.currentTime = pausePoints[destination];
+            redefinePausePoints(destination);
+        }
+
+        restartRadDemo(video);
+    }
+
+    function redefinePausePoints(index) {
+        currentPausePoint=index;
+        prevPausePoint = currentPausePoint-1;
+        nextPausePoint = currentPausePoint+1;
+    }
+
+    //Bind the spacebar to start and stop the RadDemo
+    keyboardJS.bind('space', function(e) {
+        toggleRadDemo(video);
+    });
+
+    //Bind the right arrow to go to next pause point
+    keyboardJS.bind('right', function(e) {
+        navigateRadDemo(video, nextPausePoint);
+    });
+
+    //Bind the escape key and left arrow to go back to previous pause point
+    keyboardJS.bind(['escape', 'left'], function(e) {
+        navigateRadDemo(video, prevPausePoint);
+    });
+}
